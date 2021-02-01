479d533a84180d5f24a4b8c9411180db17580e02
Keith <keith@kmddigital.com>
2021-02-01 15:38:42 -0500

    Initial Commit

---
+++ Rose Scheduling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rose Scheduling
+//
+//  Created by Keith on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
